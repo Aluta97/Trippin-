8406a7fdbc4a0a070a6ca5d1bf572d7dd020b501
Lusindiso <lusindiso@projectcodex.co>
2016-04-20 15:41:19 +0200

    final commit

--- test/trips_test.js
+++ test/trips_test.js
@@ -44,231 +44,170 @@
 
  describe("My Cape Town trips ", function() {
 
-       it("it should tell me how many trips did cape town taxis make", function() {
-         assert.equal(trips(capeTownTaxis), 54);
-       });
+   it("it should tell me how many trips did cape town taxis make", function() {
+     assert.equal(trips(capeTownTaxis), 54);
+   });
 
-       it("it should tell me the lowest number of trips taxis form Cape Town made", function() {
-         assert.equal(minimumTrips(capeTownTaxis), 9);
-       });
+   it("it should tell me the lowest number of trips taxis form Cape Town made", function() {
+     assert.equal(minimumTrips(capeTownTaxis), 9);
+   });
 
-       it("it should tell me the records for CA 123 456", function() {
+   it("it should tell me the records for CA 123 456", function() {
 
-        var expectedResults =
-          [{
-         "RegistrationNumber": "CA 123 456",
-         "Route": "Cape Town - Bellville",
-         "Fare": 13,
-         "Trips": 9
-       },
-        {
-           "RegistrationNumber": "CA 123 456",
-           "Route": "Cape Town - Gugulethu",
-           "Fare": 12,
-           "Trips": 11
-          }];
+     var expectedResults = [{
+       "RegistrationNumber": "CA 123 456",
+       "Route": "Cape Town - Bellville",
+       "Fare": 13,
+       "Trips": 9
+     }, {
+       "RegistrationNumber": "CA 123 456",
+       "Route": "Cape Town - Gugulethu",
+       "Fare": 12,
+       "Trips": 11
+     }];
 
 
-         assert.deepEqual(records(capeTownTaxis, "CA 123 456"), expectedResults);
-       });
+     assert.deepEqual(records(capeTownTaxis, "CA 123 456"), expectedResults);
+   });
 
-       it("it should tell me how many trips did CA 234 567 make", function() {
-         assert.equal(tripList(capeTownTaxis, "CA 234 567"), 11);
-       });
+   it("it should tell me how many trips did CA 234 567 make", function() {
+     assert.equal(tripList(capeTownTaxis, "CA 234 567"), 11);
+   });
 
-       it("should tell me the names of routes that CA 345 678 took", function() {
+   it("should tell me the names of routes that CA 345 678 took", function() {
 
-            expRes = ["Cape Town - Langa", "Cape Town - Cape Town"];
+     expRes = ["Cape Town - Langa", "Cape Town - Cape Town"];
 
-         assert.deepEqual(names(capeTownTaxis, "CA 345 678"), expRes);
-       });
+     assert.deepEqual(names(capeTownTaxis, "CA 345 678"), expRes);
+   });
 
-       it("should tell me the total earning of CA 234 567", function() {
-         assert.equal(earnings(capeTownTaxis, "CA 234 567" ), 132);
-       });
+   it("should tell me the total earning of CA 234 567", function() {
+     assert.equal(earnings(capeTownTaxis, "CA 234 567"), 132);
+   });
 
-       it("should tell me the total earnings for each taxi", function() {
+   it("should tell me the total earnings for each taxi", function() {
 
-           assert.deepEqual(totalEarnings(capeTownTaxis),
-           [{
-           RegistrationNumber:"CA 123 456",
-           Earning:117
-         },
-         {
-           RegistrationNumber:"CA 234 567",
-           Earning:132
-         },
-         {
-           RegistrationNumber:"CA 123 456",
-           Earning:132
-         },
-         {
-           RegistrationNumber:"CA 345 678",
-           Earning:104
-         },
-         {
-           RegistrationNumber:"CA 345 678",
-           Earning:130
-         }]
-          );
+     assert.deepEqual(totalEarnings(capeTownTaxis),
+     {
+        "CA 123 456" : 249,
+        "CA 234 567" : 132,
+        "CA 345 678" : 234
+     }
+   );
 
-       });
+   });
 
-     });
-//My durban tests::
+ });
+ //My durban tests::
 
 
-     var durbanTaxis = [
-  {
-    "RegistrationNumber": "ND 123 456",
-    "Route": "Durban - University of KZN",
-    "Fare": 7,
-    "Trips": 14
-  },
-  {
-    "RegistrationNumber": "ND 234 567",
-    "Route": "Durban - Umlazi Station",
-    "Fare": 14,
-    "Trips": 9
-  },
-  {
-    "RegistrationNumber": "ND 345 678",
-    "Route": "Durban - Umbilo",
-    "Fare": 8,
-    "Trips": 14
-  },
-  {
-    "RegistrationNumber": "ND 234 567",
-    "Route": "Durban - Umlazi Station",
-    "Fare": 14,
-    "Trips": 9
-  },
-  {
-    "RegistrationNumber": "ND 234 567",
-    "Route": "Durban - University of KZN",
-    "Fare": 7,
-    "Trips": 9
-  },
-  {
-    "RegistrationNumber": "ND 345 678",
-    "Route": "Durban - University of KZN",
-    "Fare": 7,
-    "Trips": 18
-  },
-  {
-    "RegistrationNumber": "ND 123 456",
-    "Route": "Durban - Umbilo",
-    "Fare": 8,
-    "Trips": 15
-  },
-  {
-    "RegistrationNumber": "ND 234 567",
-    "Route": "Durban - Umbilo",
-    "Fare": 8,
-    "Trips": 9
-  },
-  {
-    "RegistrationNumber": "ND 345 678",
-    "Route": "Durban - Umlazi Station",
-    "Fare": 14,
-    "Trips": 20
-  }
-];
+ var durbanTaxis = [{
+   "RegistrationNumber": "ND 123 456",
+   "Route": "Durban - University of KZN",
+   "Fare": 7,
+   "Trips": 14
+ }, {
+   "RegistrationNumber": "ND 234 567",
+   "Route": "Durban - Umlazi Station",
+   "Fare": 14,
+   "Trips": 9
+ }, {
+   "RegistrationNumber": "ND 345 678",
+   "Route": "Durban - Umbilo",
+   "Fare": 8,
+   "Trips": 14
+ }, {
+   "RegistrationNumber": "ND 234 567",
+   "Route": "Durban - Umlazi Station",
+   "Fare": 14,
+   "Trips": 9
+ }, {
+   "RegistrationNumber": "ND 234 567",
+   "Route": "Durban - University of KZN",
+   "Fare": 7,
+   "Trips": 9
+ }, {
+   "RegistrationNumber": "ND 345 678",
+   "Route": "Durban - University of KZN",
+   "Fare": 7,
+   "Trips": 18
+ }, {
+   "RegistrationNumber": "ND 123 456",
+   "Route": "Durban - Umbilo",
+   "Fare": 8,
+   "Trips": 15
+ }, {
+   "RegistrationNumber": "ND 234 567",
+   "Route": "Durban - Umbilo",
+   "Fare": 8,
+   "Trips": 9
+ }, {
+   "RegistrationNumber": "ND 345 678",
+   "Route": "Durban - Umlazi Station",
+   "Fare": 14,
+   "Trips": 20
+ }];
 
 
 
 
-     describe("My Durban taxi trips", function(){
+ describe("My Durban taxi trips", function() {
 
-       it("it should tell me how many trips did the durban taxis make", function() {
-         assert.equal(trips(durbanTaxis), 117);
-       });
+   it("it should tell me how many trips did the durban taxis make", function() {
+     assert.equal(trips(durbanTaxis), 117);
+   });
 
-       it("it should tell me the lowest number of trips taxis form Cape Town made", function() {
-         assert.equal(minimumTrips(durbanTaxis), 9);
-       });
+   it("it should tell me the lowest number of trips taxis form Cape Town made", function() {
+     assert.equal(minimumTrips(durbanTaxis), 9);
+   });
 
-        it("it should tell me the records for ND 123 456", function() {
-
-
-          var expectedResults = [{
-            "RegistrationNumber": "ND 123 456",
-            "Route": "Durban - University of KZN",
-            "Fare": 7,
-            "Trips": 14
-          },
-          {
-            "RegistrationNumber": "ND 123 456",
-            "Route": "Durban - Umbilo",
-            "Fare": 8,
-            "Trips": 15
-          }]
-          assert.deepEqual(records(durbanTaxis, "ND 123 456"), expectedResults);
-        });
-
-        it("it should tell me how many trips did ND 234 567 make", function() {
-          assert.equal(tripList(durbanTaxis, "ND 234 567"), 36);
-        });
-
-        it("should tell me the names of routes that ND 345 678 took", function() {
-
-          expRes = ["Durban - Umbilo", "Durban - University of KZN", "Durban - Umlazi Station"]
-
-          assert.deepEqual(names(durbanTaxis, "ND 345 678"), expRes);
-        });
-
-        it("should tell me the total earning of ND 234 567", function() {
-          assert.equal(earnings(durbanTaxis, "ND 234 567" ), 387);
-        });
-
-        it("should tell me the total earnings for each taxi", function() {
-
-            assert.deepEqual(totalEarnings(durbanTaxis),
+   it("it should tell me the records for ND 123 456", function() {
 
 
-           [{
-           RegistrationNumber:"ND 123 456",
-           Earning:98
-         },
-         {
-           RegistrationNumber:"ND 234 567",
-           Earning:126
-         },
-         {
-           RegistrationNumber:"ND 345 678",
-           Earning:112
-         },
-         {
-           RegistrationNumber:"ND 234 567",
-           Earning:126
-         },
-         {
-           RegistrationNumber:"ND 234 567",
-           Earning:63
-         },
-         {
-           RegistrationNumber:"ND 345 678",
-           Earning:126
-         },
-         {
-           RegistrationNumber:"ND 123 456",
-           Earning:120
-         },
-         {
-           RegistrationNumber:"ND 234 567",
-           Earning:72
-         },
-         {
-           RegistrationNumber:"ND 345 678",
-           Earning:280
-         }]
+     var expectedResults = [{
+       "RegistrationNumber": "ND 123 456",
+       "Route": "Durban - University of KZN",
+       "Fare": 7,
+       "Trips": 14
+     }, {
+       "RegistrationNumber": "ND 123 456",
+       "Route": "Durban - Umbilo",
+       "Fare": 8,
+       "Trips": 15
+     }]
+     assert.deepEqual(records(durbanTaxis, "ND 123 456"), expectedResults);
+   });
 
-          );
+   it("it should tell me how many trips did ND 234 567 make", function() {
+     assert.equal(tripList(durbanTaxis, "ND 234 567"), 36);
+   });
 
-        });
+   it("should tell me the names of routes that ND 345 678 took", function() {
 
-      });
+     expRes = ["Durban - Umbilo", "Durban - University of KZN", "Durban - Umlazi Station"]
+
+     assert.deepEqual(names(durbanTaxis, "ND 345 678"), expRes);
+   });
+
+   it("should tell me the total earning of ND 234 567", function() {
+     assert.equal(earnings(durbanTaxis, "ND 234 567"), 387);
+   });
+
+   it("should tell me the total earnings for each taxi", function() {
+
+     assert.deepEqual(totalEarnings(durbanTaxis),
+       {
+         "ND 123 456": 218,
+         "ND 234 567": 387,
+         "ND 345 678": 518
+       }
+
+     );
+
+   });
+
+ });
 
 
 
-  //   });
+ //   });
